64b6852f75cb7d5889a6d93588a2ffb765faf249
Charlie <charlie962873@gmail.com>
2025-10-12 07:53:40 +0530

    graphic designing updated

--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -38,71 +38,148 @@ export const servicesData = [
       },
     ],
   },
+  {
+    title: "Web Design",
+    description:
+      "A clunky interface can sink even the best ideas. I design responsive, pixel-perfect websites that captivate users—seamlessly blending aesthetics and usability for an exceptional browsing experience.",
+    items: [
+      {
+        title: "Restaurant UI Design ",
+        description:
+          "Created a responsive UI design for a restaurant website during a college event, earning 1st position for outstanding creativity and user experience.",
+      },
+      {
+        title: "College Website Redesign",
+        description:
+          "Designed and developed a modern, responsive UI for the college website redesign project, focusing on improved usability and visual consistency.",
+      },
+      {
+        title: "Souvenir",
+        description:
+          "Designed a custom souvenir for a freelance project, blending creative visuals with personalized branding to deliver a memorable keepsake.",
+      },
+    ],
+  },
+  {
+    title: "Graphic Design",
+    description:
+      "Created several posters for college events using canva — you can check them out using the Drive links below!",
+    items: [
+      {
+        title: "Souvenir",
+        description:
+          "Designed a custom souvenir for a freelance project, blending creative visuals with personalized branding to deliver a memorable keepsake.",
+      },
+      {
+        title: "Posters",
+        description: "Checkout the posters by clicking it !!",
+      },
+    ],
+  },
 ];
 export const projects = [
+  // {
+  //   id: 1,
+  //   name: "Shopping E-commerce Website ",
+  //   description:
+  //     "Discover the perfect fit for every wardrobe at our one-stop online fashion store! From playful styles for kids to everyday essentials and trendy picks for men and women – we bring you fashion that fits your lifestyle. Shop the latest in casuals, formals, and comfy wear for the whole family, all in one place",
+  //   href: "https://trendora-by-charlie.vercel.app",
+  //   image: "/assets/projects/trendora.png",
+  //   bgImage: "/assets/backgrounds/blanket.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "React" },
+  //     { id: 2, name: "Stripe API" },
+  //     { id: 2, name: "Razorpay API" },
+  //     { id: 4, name: "MongoDB" },
+  //     { id: 5, name: "Tailwind CSS" },
+  //   ],
+  // },
+  // {
+  //   id: 2,
+  //   name: "iPhone Website Clone",
+  //   description:
+  //     "A sleek online showcase inspired by the iPhone experience – featuring smooth GSAP animations and a clean, intuitive interface. Explore premium tech vibes with a flawless UI that feels just like scrolling through a high-end Apple site.",
+  //   href: "https://charlie-phone-x.vercel.app",
+  //   image: "/assets/projects/iphone.png",
+  //   bgImage: "/assets/backgrounds/curtains.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "React" },
+  //     { id: 2, name: "GSAP" },
+  //     { id: 3, name: "Three JS" },
+  //     { id: 4, name: "Tailwind CSS" },
+  //   ],
+  // },
+  // {
+  //   id: 3,
+  //   name: "Authentication system",
+  //   description:
+  //     "A secure MERN-based authentication platform with JWT login, protected routes, hashed passwords, and a responsive React frontend.",
+  //   href: "https://web-works-with-charlie-auth.vercel.app",
+  //   image: "/assets/projects/mern.png",
+  //   bgImage: "/assets/backgrounds/map.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "MongoDB" },
+  //     { id: 2, name: "Express" },
+  //     { id: 3, name: "React" },
+  //     { id: 4, name: "JWT" },
+  //     { id: 5, name: "Tailwind CSS" },
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   name: "BOLOBOSS-An AI Recruiter",
+  //   description:
+  //     "A SaaS-based AI interviewer platform that conducts real-time mock interviews using voice and AI. Built with Next.js, Tailwind CSS, and integrated VAPI AI for feedback and analysis.",
+  //   href: "https://boloboss-by-charlie.vercel.app",
+  //   image: "/assets/projects/boloboss.png",
+  //   bgImage: "/assets/backgrounds/map.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "Supabase" },
+  //     { id: 2, name: "NextJS" },
+  //     { id: 3, name: "Google Auth" },
+  //     { id: 4, name: "Razorpay" },
+  //     { id: 5, name: "Vapi AI" },
+  //     { id: 6, name: "OpenRouter" },
+  //   ],
+  // },
   {
     id: 1,
-    name: "Shopping E-commerce Website ",
+    name: "Responsive Restaurant UI Design ",
     description:
-      "Discover the perfect fit for every wardrobe at our one-stop online fashion store! From playful styles for kids to everyday essentials and trendy picks for men and women – we bring you fashion that fits your lifestyle. Shop the latest in casuals, formals, and comfy wear for the whole family, all in one place",
-    href: "https://trendora-by-charlie.vercel.app",
-    image: "/assets/projects/trendora.png",
+      "Created a responsive UI design for a restaurant website during a college event, earning 1st position for outstanding creativity and user experience.",
+    href: "https://www.figma.com/proto/hhodWDLoNDGPUfDk7KZtKg/RESTAURANT-UI?node-id=106-2&t=FT92emxtiUpBwMtZ-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=106%3A2&show-proto-sidebar=1",
+    image: "/assets/projects/restaurant.png",
     bgImage: "/assets/backgrounds/blanket.jpg",
-    frameworks: [
-      { id: 1, name: "React" },
-      { id: 2, name: "Stripe API" },
-      { id: 2, name: "Razorpay API" },
-      { id: 4, name: "MongoDB" },
-      { id: 5, name: "Tailwind CSS" },
-    ],
+    frameworks: [{ id: 1, name: "Figma" }],
   },
   {
     id: 2,
-    name: "iPhone Website Clone",
+    name: "College Website Redesign",
     description:
-      "A sleek online showcase inspired by the iPhone experience – featuring smooth GSAP animations and a clean, intuitive interface. Explore premium tech vibes with a flawless UI that feels just like scrolling through a high-end Apple site.",
-    href: "https://charlie-phone-x.vercel.app",
-    image: "/assets/projects/iphone.png",
-    bgImage: "/assets/backgrounds/curtains.jpg",
-    frameworks: [
-      { id: 1, name: "React" },
-      { id: 2, name: "GSAP" },
-      { id: 3, name: "Three JS" },
-      { id: 4, name: "Tailwind CSS" },
-    ],
+      "Designed and developed a modern, responsive UI for the college website redesign project, focusing on improved usability and visual consistency.",
+    href: "https://www.figma.com/proto/q764X75mi7xtuy8LftYhdm/NIT-JAMSHEDPUR?node-id=700-4299&p=f&t=iqTqqUsdx4qDrJ5F-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=700%3A4299",
+    image: "/assets/projects/college.png",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [{ id: 1, name: "Figma" }],
   },
   {
     id: 3,
-    name: "Authentication system",
+    name: "Souvenir",
     description:
-      "A secure MERN-based authentication platform with JWT login, protected routes, hashed passwords, and a responsive React frontend.",
-    href: "https://web-works-with-charlie-auth.vercel.app",
-    image: "/assets/projects/mern.png",
-    bgImage: "/assets/backgrounds/map.jpg",
-    frameworks: [
-      { id: 1, name: "MongoDB" },
-      { id: 2, name: "Express" },
-      { id: 3, name: "React" },
-      { id: 4, name: "JWT" },
-      { id: 5, name: "Tailwind CSS" },
-    ],
-  },
+      "Designed a custom souvenir for a freelance project, blending creative visuals with personalized branding to deliver a memorable keepsake.",
+    href: "https://drive.google.com/file/d/19cSsy3Phv6_LaJhsWrx5VodO039iNwEk/view?usp=sharing",
+    image: "/assets/projects/souvenir.png",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [{ id: 1, name: "Canva" }],
+  },      
   {
     id: 4,
-    name: "BOLOBOSS-An AI Recruiter",
-    description:
-      "A SaaS-based AI interviewer platform that conducts real-time mock interviews using voice and AI. Built with Next.js, Tailwind CSS, and integrated VAPI AI for feedback and analysis.",
-    href: "https://boloboss-by-charlie.vercel.app",
-    image: "/assets/projects/boloboss.png",
-    bgImage: "/assets/backgrounds/map.jpg",
-    frameworks: [
-      { id: 1, name: "Supabase" },
-      { id: 2, name: "NextJS" },
-      { id: 3, name: "Google Auth" },
-      { id: 4, name: "Razorpay" },
-      { id: 5, name: "Vapi AI" },
-      { id: 6, name: "OpenRouter" },
-    ],
+    name: "Posters",
+    description: "Checkout the posters by clicking it !!",
+    href: "https://trendora-by-charlie.vercel.app",
+    image: "/assets/projects/trendora.png",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [{ id: 1, name: "Canva" }],
   },
 ];
 export const socials = [
