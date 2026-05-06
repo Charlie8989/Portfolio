cf243c41aaa7b37ec7295631f0c3e46ce1c0339a
Charlie <charlie962873@gmail.com>
2026-05-06 22:51:48 +0530

    nextflow project added

--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -22,6 +22,11 @@ export const servicesData = [
       "A clunky interface can sink even the best ideas. I craft responsive, pixel perfect web  apps (React Native) that users love—bridging design and functionality seamlessly.",
     items: [
       {
+        title: "Nextflow",
+        description:
+          "A Clone website of Krea AI nodes feature using LLM models and more other functions",
+      },
+      {
         title: "BoloBoss – AI Interviewer",
         description:
           "SaaS-based mock AI interviewer app with real-time voice interaction, AI feedback, and payment integration.",
@@ -72,55 +77,27 @@ export const servicesData = [
   //   ],
   // },
 ];
+
 export const projects = [
   {
     id: 1,
-    name: "Shopping E-commerce Website ",
+    name: "Nextflow",
     description:
-      "Discover the perfect fit for every wardrobe at our one-stop online fashion store! From playful styles for kids to everyday essentials and trendy picks for men and women – we bring you fashion that fits your lifestyle. Shop the latest in casuals, formals, and comfy wear for the whole family, all in one place",
-    href: "https://trendora.ayush-codes.tech",
-    image: "/assets/projects/trendora.png",
-    bgImage: "/assets/backgrounds/blanket.jpg",
+      "A clone website of Krea AI nodes feature using LLM models and more other functions",
+    href: "https://nextflow.ayush-codes.tech",
+    image: "/assets/projects/nextflow.png",
+    bgImage: "/assets/backgrounds/curtains.jpg",
     frameworks: [
-      { id: 1, name: "React" },
-      { id: 2, name: "Stripe API" },
-      { id: 2, name: "Razorpay API" },
-      { id: 4, name: "MongoDB" },
-      { id: 5, name: "Tailwind CSS" },
+      { id: 1, name: "NextJS" },
+      { id: 2, name: "NodeJS" },
+      { id: 3, name: "LLM Models" },
+      { id: 4, name: "Trigger.dev" },
+      { id: 5, name: "Supabase" },
     ],
   },
   {
     id: 2,
-    name: "iPhone Website Clone",
-    description:
-      "A sleek online showcase inspired by the iPhone experience – featuring smooth GSAP animations and a clean, intuitive interface. Explore premium tech vibes with a flawless UI that feels just like scrolling through a high-end Apple site.",
-    href: "https://iPhone.ayush-codes.tech",
-    image: "/assets/projects/iphone.png",
-    bgImage: "/assets/backgrounds/curtains.jpg",
-    frameworks: [
-      { id: 1, name: "React" },
-      { id: 2, name: "GSAP" },
-      { id: 3, name: "Three JS" },
-      { id: 4, name: "Tailwind CSS" },
-    ],
-  },
-  {
-    id: 3,
-    name: "TypeWithAakash",
-    description:
-      "Typing practice web app with real-time WPM, accuracy tracking, and responsive UI.",
-    href: "https://typewithaakash.ayush-codes.tech/",
-    image: "/assets/projects/typewithaakash.png",
-    bgImage: "/assets/backgrounds/map.jpg",
-    frameworks: [
-      { id: 1, name: "NextJs" },
-      { id: 2, name: "Clerk Integrated" },
-      { id: 3, name: "Tailwind CSS" },
-    ],
-  },
-  {
-    id: 4,
-    name: "BOLOBOSS-An AI Recruiter",
+    name: "BOLOBOSS - An AI Recruiter",
     description:
       "A SaaS-based AI interviewer platform that conducts real-time mock interviews using voice and AI. Built with Next.js, Tailwind CSS, and integrated VAPI AI for feedback and analysis.",
     href: "https://boloboss.ayush-codes.tech",
@@ -135,38 +112,53 @@ export const projects = [
       { id: 6, name: "OpenRouter" },
     ],
   },
-  // {
-  //   id: 1,
-  //   name: "Responsive Restaurant UI Design ",
-  //   description:
-  //     "Created a responsive UI design for a restaurant website during a college event, earning 1st position for outstanding creativity and user experience.",
-  //   href: "https://www.figma.com/proto/hhodWDLoNDGPUfDk7KZtKg/RESTAURANT-UI?node-id=106-2&t=FT92emxtiUpBwMtZ-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=106%3A2&show-proto-sidebar=1",
-  //   image: "/assets/projects/restaurant.png",
-  //   bgImage: "/assets/backgrounds/blanket.jpg",
-  //   frameworks: [{ id: 1, name: "Figma" }],
-  // },
-  // {
-  //   id: 2,
-  //   name: "College Website Redesign",
-  //   description:
-  //     "Designed and developed a modern, responsive UI for the college website redesign project, focusing on improved usability and visual consistency.",
-  //   href: "https://www.figma.com/proto/q764X75mi7xtuy8LftYhdm/NIT-JAMSHEDPUR?node-id=700-4299&p=f&t=iqTqqUsdx4qDrJ5F-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=700%3A4299",
-  //   image: "/assets/projects/college.png",
-  //   bgImage: "/assets/backgrounds/blanket.jpg",
-  //   frameworks: [{ id: 1, name: "Figma" }],
-  // },
-  // {
-  //   id: 3,
-  //   name: "Souvenir",
-  //   description:
-  //     "Designed a custom souvenir for a freelance project, blending creative visuals with personalized branding to deliver a memorable keepsake.",
-  //   href: "https://drive.google.com/file/d/19cSsy3Phv6_LaJhsWrx5VodO039iNwEk/view?usp=sharing",
-  //   image: "/assets/projects/souvenir.png",
-  //   bgImage: "/assets/backgrounds/blanket.jpg",
-  //   frameworks: [{ id: 1, name: "Canva" }],
-  // },
-  
+  {
+    id: 3,
+    name: "Shopping E-commerce Website",
+    description:
+      "Discover the perfect fit for every wardrobe at our one-stop online fashion store! From playful styles for kids to everyday essentials and trendy picks for men and women – we bring you fashion that fits your lifestyle. Shop the latest in casuals, formals, and comfy wear for the whole family, all in one place",
+    href: "https://trendora.ayush-codes.tech",
+    image: "/assets/projects/trendora.png",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Stripe API" },
+      { id: 3, name: "Razorpay API" },
+      { id: 4, name: "MongoDB" },
+      { id: 5, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 4,
+    name: "TypeWithAakash",
+    description:
+      "Typing practice web app with real-time WPM, accuracy tracking, and responsive UI.",
+    href: "https://typewithaakash.ayush-codes.tech",
+    image: "/assets/projects/typewithaakash.png",
+    bgImage: "/assets/backgrounds/map.jpg",
+    frameworks: [
+      { id: 1, name: "NextJS" },
+      { id: 2, name: "Clerk Integrated" },
+      { id: 3, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 5,
+    name: "iPhone Website Clone",
+    description:
+      "A sleek online showcase inspired by the iPhone experience – featuring smooth GSAP animations and a clean, intuitive interface. Explore premium tech vibes with a flawless UI that feels just like scrolling through a high-end Apple site.",
+    href: "https://iphone.ayush-codes.tech",
+    image: "/assets/projects/iphone.png",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "GSAP" },
+      { id: 3, name: "Three JS" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
 ];
+
 export const socials = [
   { name: "Instagram", href: "https://www.instagram.com/ayush_h4x" },
   { name: "LinkedIn", href: "https://www.linkedin.com/in/ayush-sahu-83r/" },
